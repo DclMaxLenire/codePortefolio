772287ac94f11188643d9163fa5fa9a505b9e150
DclMaxLenire <Dcl.maximea@18pixel.fr>
2018-11-20 15:24:51 +0100

    Premier commit index terminé

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Porte-folio</title>
+</head>
+
+<body>
+<header>
+<?php include "menu.php" ; ?>
+</header>
+
+    <div class="logo">
+        <img class="logoImg" src="img/logo.png">
+    </div>
+
+    <div id="htmlCss" class="htmlCss">
+        <!-- Les projets HTML et CSS-->
+        <h2>PROJETS HTML ET CSS</h2>
+
+    <div class="imgHtmlCss">
+
+        <a href="siteHtmlCss.php"><img class="siteHtml" src="img/siteHtml.png"></a>
+        <a href="siteAccesCode.php"><img class="siteAccesCode" src="img/siteAccesCode.png"></a>
+</div>
+
+            <ul class="partieHtmlCss">
+
+                <li>Création d'un site lexique en HTML et CSS</li>
+                <li>Reproduction du site AccesCode en HTML et CSS</li>
+
+            </ul>
+    </div>
+    <div>
+  
+    </div>
+        <!-- Les projets JAVASCRIPT -->
+<div id="javaScript" class="javaScript">
+
+    <h2>PROJETS JAVASCRIPT</h2>
+
+        <div class="imgJavaScript1">
+
+            <a  href="formulaireJavaScript.php"><img class="formulaireJavaScript" src="img/formulaireJavaScript.png"></a>
+            <a  href="calculatriceJavaScript.php"><img class="calculatriceJavaScript" src="img/calculatriceJavaScript.png"></a>
+
+        </div>
+
+            <ul class="partieJavaScript1">
+
+                <li>Création d'un formulaire avec des vérifications fait en javaScript</li>
+                <li>Création d'une calculatrice en JavaScript</li>
+
+            </ul>
+</div>
+<div class="imgJavaScript2">
+        <a  href="jeuxDuShiFuMiJavaScript.php"><img class="formulaireJavaScript" src="img/jeuxDuShiFuMiJavaScript.png"></a>
+        <a href="jeuxDuPuissance4JAvaScript.php"><img  class="calculatriceJavaScript" src="img/jeuxDuPuissance4JavaScript.png"></a>
+</div>
+    <ul class="partieJavaScript2">
+
+        <li>Création du jeux du Shi-Fu-Mi en JavaScript</li>
+        <li>Création en groupe du jeux du Puissance-4 en JavaScript</li>
+
+</ul>
+        <!-- Les projets PHP-->
+<div id="php" class="php">
+    <h2>PROJETS PHP</h2>
+</div>
+
+        <div class="imgPhp">
+            
+            <a  href="calculatrice.php"><img class="tableDeMultiplicationPhp" src="img/calculatricePhp.png"></a>
+            <a  href="formulairePhp.php"><img class="formulairePhp" src="img/formulairePhp.png"></a>
+
+
+        </div>
+            <ul class="partiePhp">
+                <li>Création d'une table de multiplication en Php</li>
+                <li>Création d'un formulaire lié au Php</li>
+
+            </ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,12 @@
+<h1>Porte-folio</h1>
+        <nav>
+            <ul id="menu-demo2">
+        <!-- Menu déroulant -->
+                <li>MENU DES PROJETS</a>
+            <ul>
+                <li><a href="#htmlCss">HTML et CSS</a></li>
+                <li><a href="#javaScript">JAVASCRIPT</a></li>
+                <li><a href="#php">PHP</a></li>
+            </ul>
+</ul>
+        </nav>
