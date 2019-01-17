3117d0783a71ce32705dbc95c1b54fc510be8f04
DclMaxLenire <Dcl.maximea@18pixel.fr>
2019-01-17 15:33:10 +0100

    Version avec JS animation

---
+++ script.js
@@ -0,0 +1,101 @@
+const sr = ScrollReveal();
+
+sr.reveal('h1', {
+    distance:'50px',
+    origin: 'top',
+    duration:2000,
+    reset:true
+});
+
+sr.reveal('h2', {
+    distance:'550px',
+    origin: 'top',
+    duration:2000,
+    reset:true
+});
+
+sr.reveal('.logo', {
+    distance:'500px',
+    origin: 'top',
+    duration:2000,
+    reset:true
+});
+
+sr.reveal('.htmlCss', {
+    distance:'250px',
+    origin: 'top',
+    duration:2000,
+    reset:true
+});
+
+sr.reveal('.javaScript', {
+    distance:'250px',
+    origin: 'top',
+    duration:2000,
+    reset:true
+});
+
+sr.reveal('.php', {
+    distance:'250px',
+    origin: 'top',
+    duration:2000,
+    reset:true
+});
+
+sr.reveal('.page1', {
+    opacity:'0',
+    origin: 'bottom',
+    duration: 5500,
+    reset:true
+});
+
+sr.reveal('.page2', {
+    opacity:'0',
+    origin: 'bottom',
+    duration: 5500,
+    reset:true
+});
+
+sr.reveal('.page3', {
+    opacity:'0',
+    origin: 'bottom',
+    duration: 5500,
+    reset:true
+});
+
+sr.reveal('.page4', {
+    opacity:'0',
+    origin: 'bottom',
+    duration: 5500,
+    reset:true
+});
+
+sr.reveal('.page5', {
+    opacity:'0',
+    origin: 'bottom',
+    duration: 5500,
+    reset:true
+});
+
+sr.reveal('.page6', {
+    opacity:'0',
+    origin: 'bottom',
+    duration: 5500,
+    reset:true
+});
+
+sr.reveal('.page7', {
+    opacity:'0',
+    origin: 'bottom',
+    duration: 5500,
+    reset:true
+});
+
+sr.reveal('.page8', {
+    opacity:'0',
+    origin: 'bottom',
+    duration: 5500,
+    reset:true
+});
+
+
